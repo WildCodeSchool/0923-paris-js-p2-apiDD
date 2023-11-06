b8e825dfe5bf8a89aba799e3c04e37e24155149f
JULIEN BONET <julienbonetpro@gmail.com>
2023-11-06 20:16:07 +0100

    Equipment Bloc - desktop version ok

--- frontend/src/components/EquipmentBloc/MoneySeg/MoneySeg.jsx
+++ frontend/src/components/EquipmentBloc/MoneySeg/MoneySeg.jsx
@@ -7,18 +7,32 @@ function MoneySeg(props) {
   return (
     <div className="purse" id={id}>
       <div className="purse_change">
-        <div className="purse_value_content">
+        <div className="purse_value_content" id="purse_value_content_SmartPh">
           <p className="purse_value">{value}</p>
         </div>
+        <div className="purse_money_line_DeskT">
+          <div className="money_btn money_btn_DeskT" id="money_btn_down">
+            <p className="money_btn_content">-</p>
+          </div>
+          <div className="purse_value_content">
+            <p className="purse_value">{value}</p>
+          </div>
+          <div className="money_btn money_btn_DeskT">
+            <p className="money_btn_content" id="money_btn_up">
+              +
+            </p>
+          </div>
+        </div>
       </div>
+
       <div className="purse_title_line">
-        <div className="money_btn" id="money_btn_down">
+        <div className="money_btn money_btn_SmartPh" id="money_btn_down">
           <p className="money_btn_content">-</p>
         </div>
         <div className="purse_title_container">
           <p className="purse_title">{name}</p>
         </div>
-        <div className="money_btn">
+        <div className="money_btn money_btn_SmartPh">
           <p className="money_btn_content" id="money_btn_up">
             +
           </p>

d726ed975770f3981a68507467d6d9ac4637fbe2
JULIEN BONET <julienbonetpro@gmail.com>
2023-11-11 20:03:20 +0100

    SmartPh version ( body bckgd )  OK

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./resetStyle.css";
 import "./App.css";
-
+import "./assets/stylesheets/characterSheet_general.css";
 import CharacterInfo from "./components/Characterinfo/CharacterInfo";
 import Combat from "./components/Combat/Combat";
 import HealthDice from "./components/Combat/HealthDice";
@@ -15,16 +15,18 @@ import { CharacterProvider } from "./context/CharacterContext";
 function App() {
   return (
     <CharacterProvider>
-      <div className="result">
-        <CharacterInfo />
-        <BonusPerceptionBloc />
-        <CaracBloc />
-        <Combat />
-        {window.innerWidth < 1024 && <HealthDice />}
-        <AttackNinc />
-        <SpellBloc />
-        <EquipmentBloc />
-      </div>
+      <section id="CHARACTERS_SHEET">
+        <div className="result">
+          <CharacterInfo />
+          <BonusPerceptionBloc />
+          <CaracBloc />
+          <Combat />
+          {window.innerWidth < 1024 && <HealthDice />}
+          <AttackNinc />
+          <SpellBloc />
+          <EquipmentBloc />
+        </div>
+      </section>
     </CharacterProvider>
   );
 }
--- frontend/src/components/EquipmentBloc/EquipmentBloc.jsx
+++ frontend/src/components/EquipmentBloc/EquipmentBloc.jsx
@@ -13,7 +13,7 @@ function EquipmentBloc() {
   ];
 
   return (
-    <div className="EQUIPMENT">
+    <div id="EQUIPMENT">
       <div id="equipment_ico_">
         <img id="equipment_ico" src={Equipment} alt="equipment icone" />
       </div>

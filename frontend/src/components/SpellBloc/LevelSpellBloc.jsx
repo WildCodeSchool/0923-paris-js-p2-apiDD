29d5e59f1be6b4f83f43877b6eb3978d1d442bfc
JULIEN BONET <julienbonetpro@gmail.com>
2023-11-14 13:57:45 +0100

    spells  appli fetch and map for spell item

--- frontend/src/components/SpellBloc/LevelSpellBloc.jsx
+++ frontend/src/components/SpellBloc/LevelSpellBloc.jsx
@@ -1,12 +1,39 @@
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "./spellBloc.css";
 import LittleArrow from "../../assets/dnd_ico/fleches.png";
 
 function LevelSpellBloc(props) {
-  const { title, id, spell1, spell2, spell3 } = props;
+  const { title, id } = props;
 
+  //   gestion API
+  const [spells0, setSpells0] = useState([]);
+  const [spells1, setSpells1] = useState([]);
+  const [spells2, setSpells2] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://www.dnd5eapi.co/api/spells?level=0")
+      .then((response) => setSpells0(response.data.results))
+      .catch((error) => console.error("error fetching Spells Level 0", error));
+  }, []);
+
+  useEffect(() => {
+    axios
+      .get("https://www.dnd5eapi.co/api/spells?level=1")
+      .then((response) => setSpells1(response.data.results))
+      .catch((error) => console.error("error fetching Spells Level 0", error));
+  }, []);
+
+  useEffect(() => {
+    axios
+      .get("https://www.dnd5eapi.co/api/spells?level=2")
+      .then((response) => setSpells2(response.data.results))
+      .catch((error) => console.error("error fetching Spells Level 0", error));
+  }, []);
+
+  // gestion toggle
   const [isSpellListVisible, setSpellListVisible] = useState(false);
-
   const toggleSpellList = () => {
     setSpellListVisible(!isSpellListVisible);
   };
@@ -15,6 +42,7 @@ function LevelSpellBloc(props) {
     <div id={id} className="spells_lvls">
       <div className="spell_bloc_top_line">
         <h2 className="spell_lvl_title">{title} </h2>
+        {/* arrow for toggle */}
         {!isSpellListVisible && (
           <img
             className="arrow_btn_open"
@@ -32,27 +60,42 @@ function LevelSpellBloc(props) {
           />
         )}
       </div>
-      {isSpellListVisible && (
+
+      {/* Spells Lists by level */}
+      {title === "SPELLS - LVL 0" && isSpellListVisible && (
         <div className="spells_list">
-          <div className="spell" id={spell1.name}>
-            <div className="spell_container">
-              <p className="spell_item">{spell1.name}</p>
-            </div>
-          </div>
-          {spell2 && (
-            <div className="spell" id={spell2.name}>
-              <div className="spell_container">
-                <p className="spell_item">{spell2.name}</p>
-              </div>
-            </div>
-          )}
-          {spell3 && (
-            <div className="spell" id={spell3.name}>
-              <div className="spell_container">
-                <p className="spell_item">{spell3.name}</p>
-              </div>
-            </div>
-          )}
+          <label htmlFor="Spells_Level_0"> </label>
+          <select name="spell0" className="spell_item">
+            {spells0.map((spell0) => (
+              <option key={spell0.index} value={spell0.index}>
+                {spell0.name}
+              </option>
+            ))}
+          </select>
+        </div>
+      )}
+      {title === "SPELLS - LVL 1" && isSpellListVisible && (
+        <div className="spells_list">
+          <label htmlFor="Spells_Level_1"> </label>
+          <select name="spell1" className="spell_item">
+            {spells1.map((spell1) => (
+              <option key={spell1.index} value={spell1.index}>
+                {spell1.name}
+              </option>
+            ))}
+          </select>
+        </div>
+      )}
+      {title === "SPELLS - LVL 2" && isSpellListVisible && (
+        <div className="spells_list">
+          <label htmlFor="Spells_Level_2"> </label>
+          <select name="spell2" className="spell_item">
+            {spells2.map((spell2) => (
+              <option key={spell2.index} value={spell2.index}>
+                {spell2.name}
+              </option>
+            ))}
+          </select>
         </div>
       )}
     </div>

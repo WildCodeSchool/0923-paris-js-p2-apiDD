f8bce9599abf644e33acaaed224cdb58ac80abb5
Alex Kandel <alex.kandel@wanadoo.fr>
2023-11-16 16:57:19 +0100

    added dynamic content to armor

--- frontend/src/components/Attacks&Inc/Armors/Armor.jsx
+++ frontend/src/components/Attacks&Inc/Armors/Armor.jsx
@@ -1,14 +1,53 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "./armor.css";
 import fleche from "../../../assets/dnd_ico/fleches.png";
 
 function Armor() {
-  const [name, setName] = useState("");
+  const [armorName, setArmorName] = useState("");
   const [armorclass, setArmorclass] = useState("");
   const [strenght, setStrenght] = useState("");
   const [disadvantage, setDisadvantage] = useState("");
   const [armorIsOpen, setArmorIsOpen] = useState(false);
 
+  const [armorList, setArmorList] = useState([]);
+  useEffect(() => {
+    axios
+      .get("https://www.dnd5eapi.co/api/equipment-categories/armor")
+      .then((response) => {
+        const armorData = response.data.equipment;
+        const armorNames = armorData.map((armor) => armor.name);
+        setArmorList(armorNames);
+      })
+      .catch((error) => {
+        console.error(
+          "Erreur lors de la récupération des armes depuis l'API :",
+          error
+        );
+      });
+  }, []);
+  function OnArmorChoice(e) {
+    setArmorName(e.target.value);
+    axios
+      .get(
+        `https://www.dnd5eapi.co/api/equipment/${e.target.value
+          .replace(/ /g, "-")
+          .toLowerCase()}`
+      )
+
+      .then((response) => {
+        const armorDataChoice = response.data.armor_class;
+        const armorStat = armorDataChoice.base;
+        setArmorclass(armorStat);
+        const armorDext = armorDataChoice.dex_bonus;
+        setStrenght(armorDext);
+        const armorDataChoiceTwo = response.data.armor_class;
+        const armorDisadvantage = armorDataChoiceTwo.dex_bonus;
+        setDisadvantage(armorDisadvantage);
+        // const damageTypesThird = damagesDataThird.damage_type.name;
+        // setRangeWeaponType(damageTypesThird);
+      });
+  }
   const toggleArmor = () => {
     setArmorIsOpen(!armorIsOpen);
   };
@@ -39,13 +78,26 @@ function Armor() {
           <div className="armor_bloc_contents">
             <div>
               <div className="armor_name">
-                <input
+                {/* <input
                   className="armor_name_input"
                   type="text"
                   id="armor_name_input"
                   value={name}
-                  onChange={(e) => setName(e.target.value)}
-                />
+                  onChange={(e) => setArmorName(e.target.value)}
+                /> */}
+                <select
+                  className="armor_name_input"
+                  id="armor_name_input"
+                  value={armorName}
+                  onChange={OnArmorChoice}
+                >
+                  <option value="">Select an Armor...</option>
+                  {armorList.map((armor) => (
+                    <option key={armor} value={armor}>
+                      {armor}
+                    </option>
+                  ))}
+                </select>
               </div>
 
               <div className="class_strenght_line">
@@ -61,7 +113,7 @@ function Armor() {
                 </div>
 
                 <div className="armor_strenght">
-                  <h1 className="armor_sub_heading">Strenght</h1>
+                  <h1 className="armor_sub_heading">Dex Bonus</h1>
                   <input
                     className="armor_strenght_input"
                     type="text"
@@ -93,13 +145,19 @@ function Armor() {
             <div>
               <div className="armor_Line1">
                 <div className="armor_name_desktop">
-                  <input
-                    className="armor_name_input_desktop"
-                    type="text"
+                  <select
+                    className="armor_name_input"
                     id="armor_name_input"
-                    value={name}
-                    onChange={(e) => setName(e.target.value)}
-                  />
+                    value={armorName}
+                    onChange={OnArmorChoice}
+                  >
+                    <option value="">Select an Armor...</option>
+                    {armorList.map((armor) => (
+                      <option key={armor} value={armor}>
+                        {armor}
+                      </option>
+                    ))}
+                  </select>
                 </div>
                 <div className="armor_class_desktop">
                   <h1 className="armor_sub_heading">AC</h1>
@@ -126,7 +184,7 @@ function Armor() {
                 </div>
 
                 <div className="armor_strenght_desktop">
-                  <h1 className="armor_sub_heading">Strenght</h1>
+                  <h1 className="armor_sub_heading">Dex Bonus</h1>
                   <input
                     className="armor_strenght_input_desktop"
                     type="text"
@@ -146,13 +204,19 @@ function Armor() {
           <h1 className="armor_heading">Armor</h1>
           <div id="armor_info_content">
             <div className="armor_name">
-              <input
+              <select
                 className="armor_name_input"
-                type="text"
                 id="armor_name_input"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-              />
+                value={armorName}
+                onChange={OnArmorChoice}
+              >
+                <option value="">Select an Armor...</option>
+                {armorList.map((armor) => (
+                  <option key={armor} value={armor}>
+                    {armor}
+                  </option>
+                ))}
+              </select>
             </div>
 
             <div className="armor_class">
@@ -168,7 +232,7 @@ function Armor() {
           </div>
           <div className="armor_bloc_sec">
             <div className="armor_strenght">
-              <h1 className="armor_sub_heading">Strenght</h1>
+              <h1 className="armor_sub_heading">Dex Bonus</h1>
               <input
                 className="armor_strenght_input"
                 type="text"

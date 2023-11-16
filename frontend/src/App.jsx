53fc76b51b4240e1141a0a3644364184dddaaede
JULIEN BONET <julienbonetpro@gmail.com>
2023-11-16 12:28:09 +0100

    spell pop up completed ok

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,22 +2,21 @@ import React from "react";
 import "./resetStyle.css";
 import "./App.css";
 import "./assets/stylesheets/characterSheet_general.css";
-// import CharacterInfo from "./components/Characterinfo/CharacterInfo";
-// import Combat from "./components/Combat/Combat";
-// import HealthDice from "./components/Combat/HealthDice";
-// import CaracBloc from "./components/CaracBloc/CaracBloc";
-// import SpellBloc from "./components/SpellBloc/SpellBloc";
-// import EquipmentBloc from "./components/EquipmentBloc/EquipmentBloc";
-// import BonusPerceptionBloc from "./components/BonusPerceptionBloc/BonusPerceptionBloc";
-// import AttackNinc from "./components/Attacks&Inc/AttackNinc";
-// import SnakeDragon from "./assets/dnd_ico/snake-dragon.png";
+import CharacterInfo from "./components/Characterinfo/CharacterInfo";
+import Combat from "./components/Combat/Combat";
+import HealthDice from "./components/Combat/HealthDice";
+import CaracBloc from "./components/CaracBloc/CaracBloc";
+import SpellBloc from "./components/SpellBloc/SpellBloc";
+import EquipmentBloc from "./components/EquipmentBloc/EquipmentBloc";
+import BonusPerceptionBloc from "./components/BonusPerceptionBloc/BonusPerceptionBloc";
+import AttackNinc from "./components/Attacks&Inc/AttackNinc";
+import SnakeDragon from "./assets/dnd_ico/snake-dragon.png";
 import { CharacterProvider } from "./context/CharacterContext";
-import TestSpellBloc from "./components/SpellBloc/TestSpellBloc";
 
 function App() {
   return (
     <CharacterProvider>
-      {/* <div className="result Sheet_smartphone" id="CHARACTERS_SHEET">
+      <div className="result Sheet_smartphone" id="CHARACTERS_SHEET">
         <CharacterInfo />
         <BonusPerceptionBloc />
         <CaracBloc />
@@ -50,8 +49,7 @@ function App() {
             <EquipmentBloc />
           </div>
         </div>
-      </div> */}
-      <TestSpellBloc />
+      </div>
     </CharacterProvider>
   );
 }
--- frontend/src/components/SpellBloc/SpellPopUp/SpellPopUp.jsx
+++ frontend/src/components/SpellBloc/SpellPopUp/SpellPopUp.jsx
@@ -11,20 +11,68 @@ function SpellPopUp(props) {
     castingTime,
     description,
     damage,
+    damageSlot,
+    damagecharacLevel,
   } = props;
+
   return (
     <section id="spells_PopUp">
       <div className="spell_bloc_bkg">
         <div className="spell_pop_up_content">
           <h2 className="spell_item_title">{name}</h2>
           <img src={MagicWand} alt="Magic wand" className="MagicWand_ico" />
-          <p className="spell_item_infos">Range : {range}</p>
           <p className="spell_item_infos">
-            Components : {components && components.join(", ")}
+            &diams; Range :<span className="spell_item_italic"> {range}</span>
           </p>
-          <p className="spell_item_infos">Casting time : {castingTime}</p>
-          <p className="spell_item_infos">Duration : {duration}</p>
-          {damage && <p className="spell_item_infos">damage : {damage}</p>}
+          <p className="spell_item_infos">
+            &diams; Components :{" "}
+            <span className="spell_item_italic">
+              {" "}
+              {components && components.join(", ")}
+            </span>
+          </p>
+          <p className="spell_item_infos">
+            &diams; Casting time :{" "}
+            <span className="spell_item_italic">{castingTime}</span>
+          </p>
+          <p className="spell_item_infos">
+            &diams; Duration :{" "}
+            <span className="spell_item_italic">{duration}</span>
+          </p>
+          {damage && (
+            <p className="spell_item_infos">
+              &diams; Damage :{" "}
+              <span className="spell_item_italic">{damage}</span>
+            </p>
+          )}
+          {damageSlot && (
+            <>
+              <p className="spell_item_infos">
+                &diams; Damage at characther level:
+              </p>
+              {Object.keys(damageSlot).map((key) => {
+                return (
+                  <p key={key} className="spell_item_infos item_infos_damage">
+                    &bull; lvl {key} = {damageSlot[key]}
+                  </p>
+                );
+              })}
+            </>
+          )}
+          {damagecharacLevel && (
+            <>
+              <p className="spell_item_infos">
+                &diams; Damage at characther level:
+              </p>
+              {Object.keys(damagecharacLevel).map((key) => {
+                return (
+                  <p key={key} className="spell_item_infos item_infos_damage">
+                    &bull; lvl {key} = {damagecharacLevel[key]}
+                  </p>
+                );
+              })}
+            </>
+          )}
           <img src={MagicWand} alt="Magic wand" className="MagicWand_ico" />
           <p className="spell_item_infos">Description :</p>
           <p className="spell_item_desc">{description}</p>
